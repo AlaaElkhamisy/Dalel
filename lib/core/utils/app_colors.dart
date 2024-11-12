00b6ada551cbc7eb1019b9143efb79ed24f096da
AlaaElkhamisy <wkood1108@gmail.com>
2024-11-12 22:01:00 +0200

    App Colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,14 @@
+//abstract لان احنا مش بناخد منه instant احنا بس بنخزن فيها قيم هنستخدمها بعدين
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color primary_color = const Color(0xffc49E85);
+  static Color offwhite_color = const Color(0xffF8F4F9);
+  static Color deepbrown_color = const Color(0xff6B4B3E);
+  static Color deepgrey_color = const Color(0xff6F6460);
+  static Color lightgrey_color = const Color(0xffB4B4B4);
+  static Color grey_color = const Color(0xff999999);
+  static Color black_color = const Color(0xff333333);
+}
