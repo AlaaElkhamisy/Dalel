0df998d6b699c2cc94cf95c111edb9ee0f1a2883
AlaaElkhamisy <wkood1108@gmail.com>
2025-02-06 15:12:03 +0200

    link the app with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:dalel/core/database/cache/cache_helper.dart';
 import 'package:dalel/core/routes/app_router.dart';
 import 'package:dalel/core/services/service_locator.dart';
 import 'package:dalel/core/utils/app_colors.dart';
+import 'package:dalel/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   //قبل ما ابدا اشتغل بالlocal database هاقوله يستدعي الinit state من ال cachehelper
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setup_Service_Locator();
   //خليناها await وحطينا ال async عشان لو ال init اصلا كدا في ال cachehelper
   await getIt<CacheHelper>().init();
